3d290356686c6cf2c58d53c5629e4270fb3cbbbe
手瓜一十雪 <nanaeonn@outlook.com>
2024-02-05 15:54:15 +0800

    Init:NuCat

---
+++ NuCat/NuCat.cpp
@@ -0,0 +1,8 @@
+﻿#ifdef _WIN_PLATFORM_
+#include <Windows.h>
+int  wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd) {
+#else
+int main(int argc, char** argv){
+#endif
+	return 0;
+}
